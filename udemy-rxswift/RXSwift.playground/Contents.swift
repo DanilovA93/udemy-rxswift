cbeb947a04cb3fbe855f000616c25a11aed90450
Anton Danilov <danilovantonne@gmail.com>
2021-05-01 23:18:34 +0300

    lesson 10

--- udemy-rxswift/RXSwift.playground/Contents.swift
+++ udemy-rxswift/RXSwift.playground/Contents.swift
@@ -5,6 +5,22 @@ let observable1 = Observable.just(1)
 
 let observable2 = Observable.of(1, 2, 3)
 
-let observable3 = Observable.of([1, 2, 3])
+let observable3 = Observable.of([1, 2, 3], [3, 4, 5])
 
-let observable4 = Observable.from([1, 2, 3])
+let observable4 = Observable.from([1, 2, 3, 4 ,5])
+
+observable4.subscribe { event in
+    if let element = event.element {
+        print(element)
+    }
+}
+
+observable3.subscribe { event in
+    if let element = event.element {
+        print(element)
+    }
+}
+
+observable4.subscribe(onNext: { element in
+    print(element)
+})
